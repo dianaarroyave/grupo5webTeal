731545ba8d70ad7be4459cd517d2868d5e5491d4
dianaarroyave <93952098+dianaarroyave@users.noreply.github.com>
2022-09-02 11:01:28 -0500

    Estructura general

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require('path')
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use( express.static(publicPath) );
+
+app.listen(3000, () => console.log("servidor corriendo"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/homeSombrilla.html"));
+})
