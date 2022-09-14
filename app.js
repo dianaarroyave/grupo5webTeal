4d261ba12b1fa84e5e70f6bc6751f948f718c046
SthefaniaEcheverry <sthefaniaecheverry@gmail.com>
2022-09-14 15:06:40 -0500

    Aplicación de nodemon, enlaces corregidos y un poco de CSS

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ app.use( express.static(publicPath) );
 
 app.listen(3000, () => console.log("servidor corriendo"));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/homeSombrilla.html"));
-})
+// app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./views/homeSombrilla.html"));
+// })
 
-app.get("/carrito", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/carritoCompras.html"));
 })
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./views/formulario.login.html"));
+// })
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./views/homeCorporativo.html"));
+// })
