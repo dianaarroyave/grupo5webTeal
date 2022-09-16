1087a77c201187570eb515a82074fefefba06724
SthefaniaEcheverry <sthefaniaecheverry@gmail.com>
2022-09-16 10:43:31 -0500

    Orgenicé el archivo app.js, tabajé en el header de mobile y en mis vistas

--- app.js
+++ app.js
@@ -7,13 +7,39 @@ app.use( express.static(publicPath) );
 
 app.listen(3000, () => console.log("servidor corriendo"));
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./views/homeSombrilla.html"));
-// })
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/homeSombrilla.html"));
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
-app.get("/homesubmarcas", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/homeSubmarcas.html"));
+
+app.get("/submarcas", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/submarcas.html"));
 });
+
+app.get("/ingresar", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/formularioLogin.html"));
+});
+
+
+app.get("/registro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/formularioRegistro.html"));
+});
+
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/carritoCompras.html"));
+});
+
+
+app.get("/detalle", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/descripcionProducto.html"));
+});
+
+
+app.get("/nosotros", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/corporativo.html"));
+});
+
+app.post("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
