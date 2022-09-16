835d73af9a6776de4deef7c792d7ab13a07bf068
dianaarroyave <93952098+dianaarroyave@users.noreply.github.com>
2022-09-16 12:35:40 -0500

    Footer y descripción de producto, y root en el index

--- app.js
+++ app.js
@@ -7,14 +7,39 @@ app.use( express.static(publicPath) );
 
 app.listen(3000, () => console.log("servidor corriendo"));
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./views/homeSombrilla.html"));
-// })
 
 app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/submarcas", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/submarcas.html"));
+});
+
+app.get("/ingresar", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/formularioLogin.html"));
+});
+
+
+app.get("/registro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/formularioRegistro.html"));
+});
+
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/carritoCompras.html"));
+});
+
+
+app.get("/detalle", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/descripcionProducto.html"));
 });
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./views/homeCorporativo.html"));
-// })
+
+app.get("/nosotros", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/corporativo.html"));
+});
+
+app.post("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
