30091f32f6c7157df1b52be27612b403a04d0383
dianaarroyave <93952098+dianaarroyave@users.noreply.github.com>
2022-09-18 10:53:54 -0500

    Cambio local host a 3000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use( express.static(publicPath) );
 
-app.listen(3030, () => console.log("servidor corriendo"));
+app.listen(3000, () => console.log("servidor corriendo"));
 
 
 app.get("/", (req, res) => {
