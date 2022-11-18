27cff786b245bb53e117ba496bb5a9daf69a8dc5
dianaarroyave <93952098+dianaarroyave@users.noreply.github.com>
2022-11-18 12:27:20 -0500

    1

--- src/routes/main.js
+++ src/routes/main.js
@@ -55,9 +55,9 @@ router.post('/passwordUpdate', mainController.passwordUpdate);
 
 //middleweares
 
-router.get('/login', function(req, res, next) {
-  next()
-}, mainController.login);
+// router.get('/login', function(req, res, next) {
+//   next()
+// }, mainController.login);
 
 
 module.exports = router;
