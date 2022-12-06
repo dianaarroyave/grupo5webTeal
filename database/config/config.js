eda270a8b582534022e8aacd4628c55e41bdf7a4
dianaarroyave <93952098+dianaarroyave@users.noreply.github.com>
2022-12-05 19:43:15 -0500

    root

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,26 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "tealdb",
     "host": "127.0.0.1",
+    "port": 8889,
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_test",
     "host": "127.0.0.1",
+    "port": 8889,
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_production",
     "host": "127.0.0.1",
+    "port": 8889,
     "dialect": "mysql"
   }
 }
