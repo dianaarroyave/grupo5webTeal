d425c9cd6bba73fb73bb26356c7b74c1317c491c
dianaarroyave <93952098+dianaarroyave@users.noreply.github.com>
2022-12-11 17:38:33 -0500

    insert productos ans brands en mysql

---
+++ tealdb.sql
@@ -0,0 +1,121 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tealdb
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `brands`
+--
+
+DROP TABLE IF EXISTS `brands`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `brands` (
+  `id_brands` int unsigned NOT NULL AUTO_INCREMENT,
+  `brandName` varchar(20) NOT NULL,
+  `brandDescription` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_brands`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lineorder`
+--
+
+DROP TABLE IF EXISTS `lineorder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lineorder` (
+  `id_lineOrder` int unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` int unsigned NOT NULL,
+  `id_purchaseOrders` int unsigned NOT NULL,
+  PRIMARY KEY (`id_lineOrder`),
+  KEY `id_user_idx` (`id_user`),
+  KEY `id_purchaseOrders_idx` (`id_purchaseOrders`),
+  CONSTRAINT `id_purchaseOrders` FOREIGN KEY (`id_purchaseOrders`) REFERENCES `purchaseorders` (`id_purchaseOrders`),
+  CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id_products` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `price` int NOT NULL,
+  `productDescription` varchar(200) DEFAULT NULL,
+  `id_brands` int unsigned NOT NULL,
+  `image` varchar(45) NOT NULL,
+  `featured` binary(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_products`),
+  KEY `id_brands_idx` (`id_brands`),
+  CONSTRAINT `id_brands` FOREIGN KEY (`id_brands`) REFERENCES `brands` (`id_brands`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `purchaseorders`
+--
+
+DROP TABLE IF EXISTS `purchaseorders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchaseorders` (
+  `id_purchaseOrders` int unsigned NOT NULL AUTO_INCREMENT,
+  `id_products` int unsigned NOT NULL,
+  `quantity` int unsigned NOT NULL,
+  PRIMARY KEY (`id_purchaseOrders`),
+  KEY `id_products_idx` (`id_products`),
+  CONSTRAINT `id_products` FOREIGN KEY (`id_products`) REFERENCES `products` (`id_products`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id_users` int unsigned NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `fullName` varchar(45) NOT NULL,
+  `documentType` varchar(45) NOT NULL,
+  `documentNumber` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phoneNumber` int DEFAULT NULL,
+  `dateBirth` date DEFAULT NULL,
+  `password` varchar(20) NOT NULL,
+  `admin` tinyint unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-10 19:55:29
